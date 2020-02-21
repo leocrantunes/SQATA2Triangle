004a3c03f798588b76125e83a88751b820646e4e
Leo Antunes <leocrantunes@gmail.com>
2020-02-21 10:43:47 -0500

    Removed unused namespaces

--- SQATA2Triangle/TriangleSolver.cs
+++ SQATA2Triangle/TriangleSolver.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SQATA2Triangle
+﻿namespace SQATA2Triangle
 {
     public static class TriangleSolver
     {
